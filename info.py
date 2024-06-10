d29102a51653257a73ad5de85455c84ea6edf188
Verma7890 <137143631+Verma7890@users.noreply.github.com>
2024-06-11 04:59:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -119,7 +119,7 @@ if len(BIN_CHANNEL) == 0:
     exit()
 else:
     BIN_CHANNEL = int(BIN_CHANNEL)
-URL = environ.get("URL", "https://alexa-auto-filter-bot.onrender.com/")
+URL = environ.get("URL", "")
 if len(URL) == 0:
     print('Error - URL is missing, exiting now')
     exit()
