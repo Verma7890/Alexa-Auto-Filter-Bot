f814063467fbb8d9c067e83404a4c5be3e9aa1af
Verma7890 <137143631+Verma7890@users.noreply.github.com>
2024-07-23 15:45:15 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -198,7 +198,7 @@ async def start(client, message):
             ],[
                 InlineKeyboardButton("📍 ʜᴏᴡ ᴛᴏ ᴏᴘᴇɴ ʟɪɴᴋ 📍", url=settings['tutorial'])
             ]]
-            await message.reply(f"[{get_size(files.file_size)}] {files.file_name}\n\nYour file is ready, Please get using this link. 👍", reply_markup=InlineKeyboardMarkup(btn), protect_content=True)
+            await message.reply(f"[{get_size(files.file_size)}] {files.file_name}\n\nYour file is ready, Please get using this link. 👍", reply_markup=InlineKeyboardMarkup(btn), protect_content=False)
             return
             
     CAPTION = settings['caption']
